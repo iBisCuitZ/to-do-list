f75c447dbb589bc437a5603d5744dbf7e10be9b6
iBisCuitZ <toontown_zero@live.com>
2022-09-07 23:06:03 +0700

    XD

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,65 @@
-import React from 'react';
-import {v4 as uuidv4} from 'uuid';
-
-function Form({input, setInput, todos, setTodos}) {
+import React, { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import './Form.css';
+function FormTodo({ input, setInput, todos, setTodos, editTodo, setEditTodo }) {
   const onInputChange = (event) => {
     setInput(event.target.value);
-  }
+  };
+
+  const updateTodo = (title, id, completed) => {
+    const newTodo = todos.map((todo) =>
+      todo.id === id ? { title, id, completed } : todo
+    );
+    setTodos(newTodo);
+    setEditTodo('');
+  };
+
+  useEffect(() => {
+    if (editTodo) {
+      setInput(editTodo.title);
+    } else {
+      setInput('');
+    }
+  }, [setInput, editTodo]);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    setTodos([...todos,{id: uuidv4(), title: input, completed: false}])
-    setInput("")
-  }
+    if (!editTodo) {
+      setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+      setInput('');
+    } else {
+      updateTodo(input, editTodo.id, editTodo.completed);
+    }
+  };
+
   return (
-    <form onSubmit={onFormSubmit}>
-      <input
-        type='text'
-        placeholder='What to do'
-        className='task-input'
-        value={input}
-        required
-        onChange={onInputChange}
-      />
-      <button
-        className='button-add'
-        type='submit'
-      >
-        Add
-      </button>
-    </form>
+    <Form onSubmit={onFormSubmit}>
+      <Row>
+        <Col className='col-8'>
+          <input
+            type='text'
+            placeholder='What to do? Broh'
+            className='task-input'
+            value={input}
+            required
+            onChange={onInputChange}
+          />
+        </Col>
+        <Col className='col-2'>
+          <Button
+            className='button-add'
+            type='submit'
+          >
+            {editTodo ? 'OK' : 'Add'}
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
-export default Form;
+export default FormTodo;
